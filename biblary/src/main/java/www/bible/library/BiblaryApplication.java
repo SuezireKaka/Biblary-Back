704381c087d1530518cd9c72f1d6cc2086fb2dd2
agria <agria@HABAEK-MAIN>
2024-04-08 01:22:05 +0900

    Project start

---
+++ biblary/src/main/java/www/bible/library/BiblaryApplication.java
@@ -0,0 +1,13 @@
+package www.bible.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiblaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiblaryApplication.class, args);
+	}
+
+}
---
+++ biblary/src/test/java/www/bible/library/BiblaryApplicationTests.java
@@ -0,0 +1,13 @@
+package www.bible.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiblaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
