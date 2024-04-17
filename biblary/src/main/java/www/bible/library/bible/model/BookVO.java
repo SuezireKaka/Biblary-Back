2ed3bfab14a60b0d28920978ac4d3105d4679e64
agria <agria@HABAEK-MAIN>
2024-04-17 17:25:12 +0900

    책 데이터에 장 접미사 추가

--- biblary/src/main/java/www/bible/library/bible/model/BookVO.java
+++ biblary/src/main/java/www/bible/library/bible/model/BookVO.java
@@ -2,13 +2,16 @@ package www.bible.library.bible.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class BookVO {
 	private int pos;
 	private String shortName;
 	private String fullName;
+	private String chapterSuffix;
 	private int chapterNumber;
 	private boolean isNewTestament;
 }
